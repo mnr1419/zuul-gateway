c1c743f65956600c1f4c5155e02ef6c1b2afb614
mnr1419 <mendanarayanarao86@gmail.com>
2020-02-19 23:32:22 +0530

    intial commit

---
+++ eurekadiscoveryserver/src/test/java/com/example/eurekadiscoveryserver/EurekadiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekadiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekadiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
